2b99cf36d4413b6a6ae5b6e9f84be339eebc6361
GL62 <GL62@MSI>
2018-09-11 22:17:07 -0800

    Intial Commit

---
+++ src/dynami/program/pkg1/dinamic.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dynami.program.pkg1;
+import java.util.Scanner;
+public class dinamic {
+    public static void au(String identitas){
+        System.out.println("Identitas "+identitas);
+        
+        System.out.println("\n Hitung Fibonacci ");
+        
+        System.out.println("1,1,2,3,5,8,13,21,.....dst.\n");
+    }
+    public static int tampilinputwes(){
+        
+    }
+    public static void main(String[] args){
+        String identitas="Ahmad Dwiyan Anugrah Putra / X RPL 5/ 03";
+        au(identitas);
+    }
+}
